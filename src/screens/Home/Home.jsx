accd47aba48c232e25db56edb0b4d1b7155e0833
Hemanth Kumar <avisihks@gmail.com>
2024-08-17 13:42:01 +0530

    Add French language to Home

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -31,10 +31,15 @@ import { useNavigate } from "react-router-dom";
 import ScrollIntoView from "react-scroll-into-view";
 import ImageCarousel from "../../components/ImageCarousel/ImageCarousel";
 import ImageRotator from "../../components/ImageRotator/ImageRotator";
+import { useTranslation } from "react-i18next";
+import { useLanguage } from "../../context/LanguageContext";
 
 const Home = () => {
   const navigate = useNavigate();
   const IMAGES = [picture1, picture2, picture3, picture4];
+
+  const { t } = useTranslation();
+  const { selectedLanguage } = useLanguage();
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -120,13 +125,10 @@ const Home = () => {
         >
           <div className="flex flex-col items-start gap-5 relative self-stretch w-full flex-[0_0_auto]">
             <p className="relative self-stretch mt-[-1.00px] [font-family:'Rethink_Sans',Helvetica] font-bold text-green text-[40px] text-center tracking-[0] leading-[48.0px]">
-              Exceptional products at friendly prices
+              {t("home.title")}
             </p>
             <p className="relative self-stretch [font-family:'Rethink_Sans',Helvetica] font-normal text-green text-lg text-center tracking-[0] leading-[27px]">
-              Offer your loved ones the opportunity to discover exceptional
-              products, thanks to our group orders. Benefit from preferential
-              rates, but also enjoy together the richness and authenticity of
-              our terroirs.
+              {t("home.description")}
             </p>
           </div>
           <div className="inline-flex flex-col items-center gap-4 pt-4 pb-0 px-0 relative flex-[0_0_auto]">
@@ -135,7 +137,7 @@ const Home = () => {
               onClick={() => navigate("/auth?login")}
             >
               <button className="all-[unset] box-border relative w-fit mt-[-1.00px] ml-[-1.00px] [font-family:'Rethink_Sans',Helvetica] font-normal text-white text-base tracking-[0] leading-6 whitespace-nowrap">
-                To log in
+                {t("home.login")}
               </button>
             </button>
             <button
@@ -143,7 +145,7 @@ const Home = () => {
               onClick={() => navigate("/auth?signup")}
             >
               <span className="all-[unset] box-border relative w-fit [font-family:'Rethink_Sans',Helvetica] font-normal text-base tracking-[0] leading-6 whitespace-nowrap hover:bg-[#1b4f4a] hover:text-white cursor-pointer">
-                Create an account
+                {t("home.create_account")}
               </span>
             </button>
           </div>
@@ -162,13 +164,13 @@ const Home = () => {
         headingClassNameOverride="!tracking-[0] !text-2xl ![font-style:unset] !font-bold ![font-family:'Rethink_Sans',Helvetica] !leading-[33.6px]"
         img={frame14x10}
         imgClassName="!left-[104px]"
-        text="Savor the delights of local crafts together, in three simple steps"
-        text1="Create your ad"
-        text2="By creating an attractive ad where you define the price, give details of the products offered and introduce the artisian. All the information your potential buyers need."
-        text3="Invite your loved ones"
-        text4="Encourage them to explore the offer and place an order, all facilitated by an online platform. This is an opportunity to introduce him to . products"
-        text5="Deliver the Marchandise"
-        text6="This includes the preparation of the packages by the artisian and their delivery to you. Make sure that your loved ones receive the products under the best conditions."
+        text={t("home.layout.text")}
+        text1={t("home.layout.text1")}
+        text2={t("home.layout.text2")}
+        text3={t("home.layout.text3")}
+        text4={t("home.layout.text4")}
+        text5={t("home.layout.text5")}
+        text6={t("home.layout.text6")}
       />
       <div
         id="about"
@@ -178,32 +180,32 @@ const Home = () => {
           breakpoint="mobile"
           buttonDivClassName="!mt-[-1.00px] !text-green !ml-[-1.00px]"
           buttonStylePrimarySmallClassName="!border-white !mt-[-2.00px] !ml-[-2.00px] !flex-[0_0_auto] !bg-white"
-          buttonText="Create a good deal"
-          buttonText1="Manage my good deals"
+          buttonText={t("home.header.create_deal")}
+          buttonText1={t("home.header.manage_deal")}
           className="!flex-1 !px-5 !py-[25px] !grow !w-[unset]"
           crEzPartagezEtClassName="!tracking-[0] !text-4xl ![font-style:unset] !font-bold ![font-family:'Rethink_Sans',Helvetica] !leading-[43.2px]"
-          text="Create, Share and Enjoy with a bulk order!"
-          text1="In just a few clicks, create an order, invite your loved ones and share the pleasure of discovering together the unique flavors of local crafts. Take advantage of advantageous prices, support artisans in your region, and transform each purchase into a friendly and enriching experience."
+          text={t("home.header.text")}
+          text1={t("home.header.text1")}
           type="orange"
         />
       </div>
       <BreakpointDesktopWrapper
         breakpoint="mobile"
         buttonIcon={<IconChevronRight2 className="!relative !w-6 !h-6" />}
-        buttonText="How it works ?"
-        buttonText1="About Us"
+        buttonText={t("home.breakpoint.how_it_works")}
+        buttonText1={t("home.breakpoint.about_us")}
         className="!flex-[0_0_auto] !w-screen"
         divClassName="!tracking-[0] !text-xl ![font-style:unset] !font-bold ![font-family:'Rethink_Sans',Helvetica] !leading-[28.0px]"
         headingClassName="!tracking-[0] !text-4xl ![font-style:unset] !font-bold ![font-family:'Rethink_Sans',Helvetica] !leading-[43.2px]"
         headingClassNameOverride="!tracking-[0] !text-xl ![font-style:unset] !font-bold ![font-family:'Rethink_Sans',Helvetica] !leading-[28.0px]"
         placeholderImage={placeHolderImage12}
-        text="Our approach"
-        text1="Embark on the adventure of artisanal flavors"
-        text2="By joining our group order community, benefit from advantageous prices while sharing the pleasure of tasting the best of local crafts. Make each purchase a unique experience, supporting artisans and sharing moments with your loved ones."
-        text3="Shared consumption experience"
-        text4="An enriching social experience, strengthening bonds with family and friends while discovering exceptional products together."
-        text5="Support and promotion of local artisans"
-        text6="By favoring products from local crafts, you contribute to maintaining and promoting the unique know-how of our regions."
+        text={t("home.breakpoint.text")}
+        text1={t("home.breakpoint.text1")}
+        text2={t("home.breakpoint.text2")}
+        text3={t("home.breakpoint.text3")}
+        text4={t("home.breakpoint.text4")}
+        text5={t("home.breakpoint.text5")}
+        text6={t("home.breakpoint.text6")}
       />
       <div
         id="faq"
@@ -212,49 +214,49 @@ const Home = () => {
         <Faq
           breakpoint="mobile"
           buttonStylePrimarySmallClassName="!self-stretch !mr-[-1.00px] !ml-[-1.00px] !mb-[-1.00px] !flex-[0_0_auto] !flex !w-full"
-          buttonText="Questions ? . Contact us"
+          buttonText={t("home.faq.button_text")}
           className="!flex-1 !px-5 !py-[25px] !grow !w-[unset]"
           divClassName="!tracking-[0] !text-4xl ![font-style:unset] !font-bold ![font-family:'Rethink_Sans',Helvetica] !leading-[43.2px]"
           questionClassName="!mt-[-1.00px]"
-          text="You will find answers to the most common questions regarding our bulk ordering platform for artisan products."
-          que1="How does bulk ordering work on your platform?"
-          que2="Who are the partner artisans?"
-          que3="How do I invite my loved ones to join a group order?"
-          que4="How is the price of products calculated?"
-          que5="How does delivery work?"
-          ans1="This is a very long answer for the question from the faqlistmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
-          ans2="This is a very long answer for the question from the faqlistmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
-          ans3="This is a very long answer for the question from the faqlist"
-          ans4="This is a very long answer for the question from the faqlistmnnnnnnnnnnnnnnn"
-          ans5="This is a very long answer for the question from the faqlistnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn"
+          text={t("home.faq.text")}
+          que1={t("home.faq.que1")}
+          que2={t("home.faq.que2")}
+          que3={t("home.faq.que3")}
+          que4={t("home.faq.que4")}
+          que5={t("home.faq.que5")}
+          ans1={t("home.faq.ans1")}
+          ans2={t("home.faq.ans2")}
+          ans3={t("home.faq.ans3")}
+          ans4={t("home.faq.ans4")}
+          ans5={t("home.faq.ans5")}
         />
       </div>
       <Footer
         breakpoint="mobile"
-        buttonText="Subscribe"
+        buttonText={t("home.footer.subscribe")}
         className="!flex-[0_0_auto] !w-screen"
         divClassName="![white-space:unset] !w-[155px]"
         linksClassName="!h-[485px] !flex-[unset]"
         spanClassName="!tracking-[0] !text-xs !font-normal ![font-family:'Rethink_Sans',Helvetica] !leading-[18px]"
         spanClassName1="!tracking-[0] !text-xs !font-normal ![font-family:'Rethink_Sans',Helvetica] !leading-[18px]"
         spanClassNameOverride="!text-[unset]"
-        text="Sitemap"
-        text1="About"
-        text10="Our committed newsletter"
-        text11="Stay informed (only once a month) of our news"
-        text12="By subscribing, you agree to our "
-        text13=" and consent to receive updates from our company."
-        text14="© 2024 The Good Deals. All rights reserved."
-        text2="How it works ?"
-        text3="Bulk order"
-        text4="Support / Contact"
-        text5="FAQs"
-        text6="Information"
-        text7="Legal Notice"
-        text8="Privacy Policy"
-        text9="General Conditions of Sales and Use"
+        text={t("home.footer.sitemap")}
+        text1={t("home.footer.about")}
+        text2={t("home.footer.how_it_works")}
+        text3={t("home.footer.bulk_order")}
+        text4={t("home.footer.support_contact")}
+        text5={t("home.footer.faq")}
+        text6={t("home.footer.information")}
+        text7={t("home.footer.legal_notice")}
+        text8={t("home.footer.privacy_policy")}
+        text9={t("home.footer.general_conditions")}
+        text10={t("home.footer.newsletter_title")}
+        text11={t("home.footer.newsletter_description")}
+        text12={t("home.footer.subscribe_agreement")}
+        text13={t("home.footer.agree")}
+        text14={t("home.footer.copyright")}
         textInputDivClassName="!mt-[-1.00px] !text-[#4f4f4f] !ml-[-1.00px]"
-        textInputText="Enter your email"
+        textInputText={t("home.footer.enter_email")}
         textInputTextInputClassName="!self-stretch !border-white !rounded-[100px] !mr-[-2.00px] !mt-[-2.00px] !ml-[-2.00px] !flex-[0_0_auto] !w-full"
       />
     </div>
