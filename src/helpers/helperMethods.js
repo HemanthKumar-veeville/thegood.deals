16b4c0981968e1e141a1f044d57154be5ce1ba91
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-09-15 22:36:16 +0530

    Update helpers

--- src/helpers/helperMethods.js
+++ src/helpers/helperMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL = "https://13.39.41.62/";
-// const BASE_URL = "https://5905-49-205-86-76.ngrok-free.app/";
+const BASE_URL = "/api/";
+
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
