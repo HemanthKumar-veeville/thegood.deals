3edc6dca7b67656c34ffc07f6cb7667b9079548f
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-10-07 16:42:49 +0530

    Update Charges

--- src/screens/ActiveDeal/ActiveDeal.jsx
+++ src/screens/ActiveDeal/ActiveDeal.jsx
@@ -149,63 +149,48 @@ const ActiveDeal = () => {
                 </div>
               </div>
             </div>
-            {Array.isArray(chargeStats) &&
-              chargeStats?.length ===
-                chargeStats?.map(
-                  (charge) => charge?.payment_status === "succeeded"
-                )?.length && (
-                <div className="flex items-start gap-[25px] px-[18px] py-[15px] relative self-stretch w-full flex-[0_0_auto] bg-greengreen-light-6 rounded-lg">
-                  <div className="flex items-center gap-3 relative flex-1 grow">
-                    <div className="relative w-5 h-5 bg-greengreen rounded-[10px]">
-                      <CheckmarkCircle
-                        className="!absolute !w-3 !h-3 !top-1 !left-1"
-                        color="white"
-                      />
-                    </div>
-                    <p className="relative flex-1 mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-[#004434] text-sm tracking-[0] leading-5">
-                      <span className="font-bold">{`The deal is over.`}</span>
-                    </p>
+            {Array.isArray(chargeStats) && chargeStats?.length > 0 && (
+              <div className="flex items-start gap-[25px] px-[18px] py-[15px] relative self-stretch w-full flex-[0_0_auto] bg-greengreen-light-6 rounded-lg">
+                <div className="flex items-center gap-3 relative flex-1 grow">
+                  <div className="relative w-5 h-5 bg-greengreen rounded-[10px]">
+                    <CheckmarkCircle
+                      className="!absolute !w-3 !h-3 !top-1 !left-1"
+                      color="white"
+                    />
                   </div>
+                  <p className="relative flex-1 mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-[#004434] text-sm tracking-[0] leading-5">
+                    <span className="font-bold">{`The deal is over.`}</span>
+                  </p>
                 </div>
-              )}
-            {Array.isArray(chargeStats) &&
-              chargeStats?.length !==
-                chargeStats?.map(
-                  (charge) => charge?.payment_status === "succeeded"
-                )?.length &&
-              dealData?.deal_ends_in > 0 && (
-                <div className="flex items-start gap-[25px] px-[18px] py-[15px] relative self-stretch w-full flex-[0_0_auto] bg-greengreen-light-6 rounded-lg">
-                  <div className="flex items-center gap-3 relative flex-1 grow">
-                    <div className="relative w-5 h-5 bg-greengreen rounded-[10px]">
-                      <ClockAlt13
-                        className="!absolute !w-3 !h-3 !top-1 !left-1"
-                        color="white"
-                      />
-                    </div>
-                    <p className="relative flex-1 mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-[#004434] text-sm tracking-[0] leading-5">
-                      <span className="[font-family:'Inter',Helvetica] font-normal text-[#004434] text-sm tracking-[0] leading-5">
-                        {t("active_deal.deal_ends_in")}
-                        <br />
-                      </span>
-                      <span className="font-bold">
-                        {`${dealData?.deal_ends_in} days and 6 hours`}
-                      </span>
-                    </p>
+              </div>
+            )}
+            {dealData?.deal_ends_in > 0 && (
+              <div className="flex items-start gap-[25px] px-[18px] py-[15px] relative self-stretch w-full flex-[0_0_auto] bg-greengreen-light-6 rounded-lg">
+                <div className="flex items-center gap-3 relative flex-1 grow">
+                  <div className="relative w-5 h-5 bg-greengreen rounded-[10px]">
+                    <ClockAlt13
+                      className="!absolute !w-3 !h-3 !top-1 !left-1"
+                      color="white"
+                    />
                   </div>
+                  <p className="relative flex-1 mt-[-1.00px] [font-family:'Inter',Helvetica] font-normal text-[#004434] text-sm tracking-[0] leading-5">
+                    <span className="[font-family:'Inter',Helvetica] font-normal text-[#004434] text-sm tracking-[0] leading-5">
+                      {t("active_deal.deal_ends_in")}
+                      <br />
+                    </span>
+                    <span className="font-bold">
+                      {`${dealData?.deal_ends_in} days and 6 hours`}
+                    </span>
+                  </p>
                 </div>
-              )}
-            {Array.isArray(chargeStats) &&
-              chargeStats?.length !==
-                chargeStats?.map(
-                  (charge) => charge?.payment_status === "succeeded"
-                )?.length && (
-                <DangerAlert
-                  alertMessage="The payment of one of the participants could not be made."
-                  participants={
-                    (Array.isArray(chargeStats) && chargeStats) || []
-                  }
-                />
-              )}
+              </div>
+            )}
+            {Array.isArray(chargeStats) && chargeStats?.length > 0 && (
+              <DangerAlert
+                alertMessage="The payment of one of the participants could not be made."
+                participants={(Array.isArray(chargeStats) && chargeStats) || []}
+              />
+            )}
             <div onClick={handleOrder}>
               <ImageSlider pictures={dealData?.deal_images || [blogImage]} />
             </div>
