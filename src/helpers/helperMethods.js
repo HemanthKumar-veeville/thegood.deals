d2dce477d11946628d57454c9e074d35f8cdb735
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-11-13 17:54:35 +0530

    Update prod url

--- src/helpers/helperMethods.js
+++ src/helpers/helperMethods.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import i18next from "i18next";
 import { loadStripe } from "@stripe/stripe-js";
-// Base URL for your API
-const BASE_URL = "https://thegood.deals/api";
+import { BASE_URL } from "../config";
 
 // Get current language from i18next
 const currentLanguage = i18next.language || "fr"; // Default to 'en-US' if no language is set
