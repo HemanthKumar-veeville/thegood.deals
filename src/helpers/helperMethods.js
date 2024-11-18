6c4c2133e27da15a2f8e52086aabf369f1864b84
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-11-18 13:53:00 +0530

    Update icons

--- src/helpers/helperMethods.js
+++ src/helpers/helperMethods.js
@@ -18,7 +18,6 @@ export const axiosInstance = axios.create({
     "Accept-Language": currentLanguage, // Dynamically set Accept-Language header
   },
   withCredentials: true,
-  timeout: 30000,
 });
 
 export const createStripeAccount = async (email, dealId) => {
