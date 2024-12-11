9ef9eb589d9c0e585617c324d7b251e399201376
HemanthKumar-veeville <hemanth.kumar@veeville.com>
2024-12-11 12:03:19 +0530

    Update orders

--- src/screens/Orders/Orders.jsx
+++ src/screens/Orders/Orders.jsx
@@ -168,12 +168,9 @@ const Orders = ({ dealId, dealType }) => {
                           key={order.order_id}
                           className="flex flex-col items-start gap-4 pt-0 pb-4 px-4 bg-whitewhite mt-3 w-full"
                         >
-                          {order.payment_status !== null &&
-                            order.payment_status !== "succeeded" && (
-                              <CustomStatus
-                                text={t("orders.awaiting_payment")}
-                              />
-                            )}
+                          {order.payment_status !== "succeeded" && (
+                            <CustomStatus text={t("orders.awaiting_payment")} />
+                          )}
                           {/* Products in Each Order */}
                           {order?.products?.map((product, index) => (
                             <div
@@ -244,30 +241,29 @@ const Orders = ({ dealId, dealType }) => {
                             </div>
                           </div>
                           <Line />
-                          {order.payment_status !== null &&
-                            order.payment_status !== "succeeded" && (
-                              <>
-                                <div
-                                  className="mt-3 flex items-center justify-center gap-2.5 px-6 py-3 relative self-stretch w-full flex-[0_0_auto] bg-primary-color rounded-md"
-                                  onClick={() =>
-                                    handleEditPayment(order.order_id)
-                                  }
+                          {order.payment_status !== "succeeded" && (
+                            <>
+                              <div
+                                className="mt-3 flex items-center justify-center gap-2.5 px-6 py-3 relative self-stretch w-full flex-[0_0_auto] bg-primary-color rounded-md"
+                                onClick={() =>
+                                  handleEditPayment(order.order_id)
+                                }
+                              >
+                                <button
+                                  type="submit"
+                                  className="all-[unset] box-border relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-medium text-white text-base text-center tracking-[0] leading-6 whitespace-nowrap"
                                 >
-                                  <button
-                                    type="submit"
-                                    className="all-[unset] box-border relative w-fit mt-[-1.00px] [font-family:'Inter',Helvetica] font-medium text-white text-base text-center tracking-[0] leading-6 whitespace-nowrap"
-                                  >
-                                    {t("orders.edit_payment")}{" "}
-                                    {/* Translated button text */}
-                                  </button>
-                                  <ArrowRight1
-                                    className="!relative !w-5 !h-5"
-                                    color="white"
-                                  />
-                                </div>
-                                <Line />
-                              </>
-                            )}
+                                  {t("orders.edit_payment")}{" "}
+                                  {/* Translated button text */}
+                                </button>
+                                <ArrowRight1
+                                  className="!relative !w-5 !h-5"
+                                  color="white"
+                                />
+                              </div>
+                              <Line />
+                            </>
+                          )}
                         </div>
                       ))}
                     </div>
